526bda9e52ff4199c3b6fe26ac79eb831c22a1ed
Ivan Gaenko <gaenko37@gmail.com>
2018-11-28 01:33:47 +0200

    game

--- main.js
+++ main.js
@@ -1,160 +1,122 @@
 let ork = {
-  title: "ork",
+  title: "Ork",
   name: "Zarzul",
   health: 120,
-  damageLow: 10,
-  damageMedium: 20,
-  damageHeavy: 30
+  damage : function() {
+    let arrDamage = [0, 10, 20, 30];
+    let chooseRandom = Math.round(Math.random() * 3);
+    let damageOrkRandom = arrDamage[chooseRandom];
+    return damageOrkRandom;
+  }
 };
 
 let vampire = {
-  title: "vampire",
+  title: "Vampire",
   name: "Polym",
-  health: 85,
-  damageLow: 5,
-  damageMedium: 15,
-  damageHeavy: 40
-};
-
-let human = {
-  title: "human",
-  name: "Igor",
   health: 100,
-  damageLow: 10,
-  damageMedium: 20,
-  damageHeavy: 30
+  damage : function() {
+    let arrDamage = [0, 15, 20, 40];
+    let chooseRandom = Math.round(Math.random() * 3);
+    let damageVampireRandom = arrDamage[chooseRandom];
+    return damageVampireRandom;
+  }
 };
 
-class Damage {
+class Enemy {
   constructor(obj) {
     this.state = obj;
-  }
+  };
+
+  start() {
+    return `<div class="start">My name is ${this.state.name}, and my health is ${this.state.health} points.</div>`;
+  };
+
+  strikeFinish() {
+    document.getElementById("strikeFirstEnemy").onclick = "";
+    document.getElementById("strikeSecondEnemy").onclick = "";
+    let createFinishElem = document.createElement("div");
+    createFinishElem.innerHTML = `<div class="finish-bar">Finish! ${this.state.name} wins!</div>`;
+    finishBar.appendChild(createFinishElem);
+    return "";
+  };
 }
 
-class FirstEnemy extends Damage {
+class Ork extends Enemy {
   constructor(obj) {
     super();
     this.state = obj;
-  }
-  start() {
-    return `<div>
-        My name is ${this.state.name}, and my health is ${this.state.health}
-      </div>`;
-  }
+  };
+
   strike() {
-    `${(secondEnemy.state.health -= this.state.damageLow)}`;
-    if (
-      `${secondEnemy.state.health}` > 0 &&
-      !(`${secondEnemy.state.health - this.state.damageLow}` < 0)
-    ) {
+    if ( `${secondEnemy.state.health}` > 0 && !(`${this.vampireLeftLife}` == 0)) {
       return this.strikeSucces();
     } else {
-      return this.strikeFalse();
+      return this.strikeFinish();
     }
-  }
-  strikeSucces() {
-    return `<div>
-      ${this.state.title} ${this.state.name} strikes ${
-      secondEnemy.state.title
-    } ${secondEnemy.state.name} in ${this.state.damageLow} points
-    </div>
-     <div>Health of ${secondEnemy.state.name} is ${
-      secondEnemy.state.health
-    }</div>`;
-  }
-  strikeFalse() {
-    return `<div>Finish! ${firstEnemy.state.name} wins!</div>`;
-  }
-}
+  };
 
-class SecondEnemy extends Damage {
+  strikeSucces() {
+    let orkDamageDone = `${this.state.damage()}`;
+    let vampireLeftLife = `${secondEnemy.state.health -= orkDamageDone}`;
+    if (vampireLeftLife < 0) {
+      vampireLeftLife = 0;
+    };
+    return `<div class="leftContent">
+      ${this.state.title} ${this.state.name} strikes ${secondEnemy.state.title} ${secondEnemy.state.name} in ${orkDamageDone} points</div>
+      <div class="leftContent">Health of ${secondEnemy.state.name} is ${vampireLeftLife}</div>`;
+  };
+};
+
+class Vampire extends Enemy {
   constructor(obj) {
     super();
     this.state = obj;
-  }
-  start() {
-    return `My name is ${this.state.name}, and my health is ${
-      this.state.health
-    }`;
-  }
+  };
+
   strike() {
-    `${(firstEnemy.state.health -= this.state.damageLow)}`;
-    if (
-      `${firstEnemy.state.health}` > 0 &&
-      !(`${firstEnemy.state.health - this.state.damageLow}` < 0)
-    ) {
+    if (`${firstEnemy.state.health}` > 0 && !(`${this.orkLeftLife}` == 0)) {
       return this.strikeSucces();
     } else {
-      return this.strikeFalse();
+      return this.strikeFinish();
     }
-  }
+  };
+
   strikeSucces() {
-    return `<div>
-        ${this.state.title} ${this.state.name} strikes ${
-      firstEnemy.state.title
-    } ${firstEnemy.state.name} in ${this.state.damageLow} points
-      </div>
-       <div>Health of ${firstEnemy.state.name} is ${
-      firstEnemy.state.health
-    }</div>`;
-  }
-  strikeFalse() {
-    return `<div>Finish! ${secondEnemy.state.name} wins!</div>`;
-  }
-}
+    let vampireDamageDone = `${this.state.damage()}`;
+    let orkLeftLife = `${firstEnemy.state.health -= vampireDamageDone}`;
+    if (orkLeftLife < 0) {
+      orkLeftLife = 0;
+    };
+    return `<div class="rightContent">
+      ${this.state.title} ${this.state.name} strikes ${firstEnemy.state.title} ${firstEnemy.state.name} in ${vampireDamageDone} points</div>
+      <div class="rightContent">Health of ${firstEnemy.state.name} is ${orkLeftLife}</div>`;
+  };
+};
 
-// let chooseOne;
 
-// let chooseTwo;
+let firstEnemy = new Ork(ork);
+let secondEnemy = new Vampire(vampire);
 
-// chooseFirstEnemy.onclick = function() {
-//   let arr = [ork, vampire, human];
-//   let chooseRandom = Math.round(Math.random() * 2);
-//   chooseOne = `${arr[chooseRandom].title}`;
-//   console.log(
-//     `You choose first enemy: ${arr[chooseRandom].title} - ${
-//       arr[chooseRandom].name
-//     }`
-//   );
-// };
-
-// chooseSecondEnemy.onclick = function() {
-//   let arr = [ork, vampire, human];
-//   let chooseRandom = Math.round(Math.random() * 2);
-//   chooseTwo = arr[chooseRandom].title;
-//   console.log(
-//     `You choose second enemy: ${arr[chooseRandom].title} - ${
-//       arr[chooseRandom].name
-//     }`
-//   );
-//   return chooseTwo;
-// };
-
-let firstEnemy = new FirstEnemy(ork);
-let secondEnemy = new SecondEnemy(vampire);
-
-// buttstart.onclick = () => {
-//   firstEnemy.start();
-//   secondEnemy.start();
-// };
-
-buttStart.onclick = function() {
+function buttStart() {
   let createStartFirstElem = document.createElement("div");
   let createStartSecondElem = document.createElement("div");
   createStartFirstElem.innerHTML = firstEnemy.start();
   createStartSecondElem.innerHTML = secondEnemy.start();
   startBar.appendChild(createStartFirstElem);
   startBar.appendChild(createStartSecondElem);
+  document.getElementById("buttStart").onclick = "";
 };
 
-strikeFirstEnemy.onclick = function() {
+function strikeFirstEnemy() {
   let createFirstElem = document.createElement("div");
   createFirstElem.innerHTML = firstEnemy.strike();
   leftContent.appendChild(createFirstElem);
+  document.getElementById("buttStart").onclick = "";
 };
 
-strikeSecondEnemy.onclick = function() {
+function strikeSecondEnemy() {
   let createSecondElem = document.createElement("div");
   createSecondElem.innerHTML = secondEnemy.strike();
   rightContent.appendChild(createSecondElem);
+  document.getElementById("buttStart").onclick = "";
 };
