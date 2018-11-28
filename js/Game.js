5b804ef1c7a4a3f9ffba973b8e61c7e4f39c72d8
Vadim Bilyuk <vabilyuk@gmail.com>
2018-11-28 03:00:49 +0200

    game is looks ready :c

--- js/Game.js
+++ js/Game.js
@@ -1,109 +1,278 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
 class Creature {
   constructor() {
-    this.state = {
-      healthPoints: 100,
-      manaPoints: 100,
-    };
+    this.healthPoints = 100;
+    this.manaPoints = 100;
+    this.level = 1;
+    this.isAlive = true;
+  }
+
+  checkAlive() {
+    if (this.healthPoints <= 0) this.isAlive = false;
   }
 }
 
 class Hero extends Creature {
-  constructor(name = 'Nameless godslayer') {
+  constructor() {
     super();
-    this.state = {
-      name,
-      race: 'human',
-      weapon: 'Fists',
-      level: 1,
-      isDead: false,
+    this.information = {
+      name: 'Nameless',
+      profession: 'Noob',
+    };
+    this.ammunition = {
+      firstWeapon: 'Fists',
+      secondWeapon: 'Fists',
+      arrows: 0,
+      healingPotion: 3,
+      manaPotion: 3,
     };
   }
 
-  isAlive() {
-    if (this.state.healthPoints <= 0) {
-      this.state.isDead = true;
-      console.log('Ты погиб !');
+  attack() {
+    this.levelUp();
+
+    switch (this.information.profession) {
+      case 'Mage' && this.manaPoints >= 10:
+        this.manaPoints -= 10;
+        return 50;
+
+      case 'Mage' && this.ammunition.manaPotion > 0:
+        const isWannaDrink = confirm('Looks like you have no mp for attack with magic\nWould you like to drink mana potion?');
+        if (isWannaDrink) {
+          this.drinkManaPot();
+          this.manaPoints -= 10;
+        }
+        return 50;
+
+      case 'Archer' && this.ammunition.arrows >= 1:
+        this.ammunition.arrows -= 1;
+        return 50;
+
+      case 'Archer':
+        return 20;
+
+      case 'Noob':
+        return 20;
+
+      default:
+        return 50;
     }
   }
 
-  render() {
-    return `<div class="hero-info><h4>Name: ${this.state.name}</h4><h4>Level: ${this.state.level}</h4><h4>Weapon: ${this.state.weapon}</h4></div>`;
+  levelUp() {
+    this.level = game.state.killCount + 1;
+  }
+
+  drinkHealingPot() {
+    if (this.ammunition.healingPotion >= 1) {
+      this.ammunition.healingPotion -= 1;
+      this.healthPoints = 100;
+      console.log('_____________________________________');
+      console.log('You drank a potion. Your HP - 100 now !');
+      console.log('_____________________________________');
+    } else {
+      console.log('_____________________________________');
+      console.log('WARNING');
+      console.log('Looks like you have no healing potion');
+      console.log('_____________________________________');
+    }
+  }
+
+  drinkManaPot() {
+    if (this.ammunition.manaPotion >= 1) {
+      this.ammunition.manaPotion -= 1;
+      this.manaPoints = 100;
+      console.log('_____________________________________');
+      console.log('You drank a potion. Your MP - 100 now !');
+      console.log('_____________________________________');
+    } else {
+      console.log('_____________________________________');
+      console.log('WARNING');
+      console.log('Looks like you have no mana potion');
+      console.log('_____________________________________');
+    }
+  }
+
+  setupHero() {
+    this.getFirstWeapon();
+    this.getSecondWeapon();
+    this.getArrows();
+  }
+
+  getFirstWeapon() {
+    switch (this.information.profession) {
+      case 'Warrior':
+        this.ammunition.firstWeapon = 'Broken sword';
+        break;
+      case 'Scout':
+        this.ammunition.firstWeapon = 'Blunt knife';
+        break;
+      case 'Archer':
+        this.ammunition.firstWeapon = 'Toy bow';
+        break;
+      case 'Mage':
+        this.ammunition.firstWeapon = 'Broken staff';
+        break;
+      default:
+        this.ammunition.firstWeapon = 'Fists';
+        break;
+    }
+  }
+
+  getSecondWeapon(profession) {
+    switch (profession) {
+      case 'Archer':
+        this.ammunition.secondWeapon = 'Giat toothpick';
+        break;
+      default:
+        this.ammunition.secondWeapon = 'Fists';
+        break;
+    }
+  }
+
+  getArrows() {
+    switch (this.information.profession) {
+      case 'Archer':
+        this.ammunition.arrows = 30;
+        break;
+      default:
+        this.ammunition.arrows = 0;
+        break;
+    }
   }
 }
 
 class Enemy extends Creature {
-  constructor(name) {
+  constructor() {
     super();
-    this.state = {
-      name,
-      isDead: false,
+    this.information = {
+      name: 'Silly monkey',
     };
   }
 
-  battle() {
-    const tempRandom = Math.floor(Math.random() * 10);
-    if (tempRandom < 2) {
-      console.log(`${this.state.name} смог атаковать тебя`);
-      hero.state.healthPoints -= 10;
-      console.log(`Твое здоровье опустилось до ${hero.state.healthPoints}`);
-    } else {
-      console.log(`${this.state.name} промахнулся ! Самое время атаковать его в ответ.`);
-      console.log('Ты успешно контратаковал противника !');
-      enemy.state.healthPoints -= 50;
-      console.log(`Его здоровье опустилось до ${enemy.state.healthPoints}`);
-      this.isAlive();
-    }
-  }
-
-  isAlive() {
-    if (this.state.healthPoints <= 0) {
-      this.state.isDead = true;
-      console.log('Противник уничтожен !');
-    }
+  attack() {
+    return 5;
   }
 }
 
+// Creating instances
+const hero = new Hero();
+const enemy = new Enemy();
 
 class Game {
   constructor() {
     this.state = {
       currentStep: 0,
-      isInBattle: false,
+      killCount: 0,
     };
   }
 
   getName() {
-    const name = prompt('Tell me your name');
-    hero.state.name = name;
+    hero.information.name = prompt('Please enter your hero\'s name ', 'Nameless');
+    this.getProf();
+  }
+
+  getProf() {
+    hero.information.profession = prompt('Please choose your profession from this 4:\nWarrior\nScout\nArcher\nMage', 'Noob');
+  }
+
+  welcomeMessage() {
+    this.getName();
+    hero.setupHero();
+    console.log(`Welcome to our dungeon |${hero.information.name}|`);
+    console.log('Just chill and watch your hero');
+    this.statMessage();
+    this.moveForward();
+  }
+
+  statMessage() {
+    console.log('_____________________________________');
+    console.log('Your current stats:');
+    console.log(`Your Level - ${hero.level}`);
+    console.log(`Your HP - ${hero.healthPoints}, MP = ${hero.manaPoints}`);
+    console.log(`Your Profession - ${hero.information.profession}`);
+    console.log('Your Ammunition:');
+    console.log(`First weapon: ${hero.ammunition.firstWeapon}`);
+    console.log(`Second weapon: ${hero.ammunition.secondWeapon}`);
+    console.log(`Arrows: ${hero.ammunition.arrows}`);
+    console.log(`Healing potion: ${hero.ammunition.healingPotion} and Mana potion: ${hero.ammunition.manaPotion}`);
+    console.log('_____________________________________');
+  }
+
+  moveForward() {
+    setTimeout(() => {
+      this.state.currentStep += 1;
+      console.log('_____________________________________');
+      console.log('You are going forward !');
+      console.log('_____________________________________');
+      if (this.state.currentStep >= 50) {
+        console.log('_____________________________________');
+        console.log('_____________________________________');
+        console.log('_____________________________________');
+        console.log('| You went through the dungeon ! |');
+        console.log('_____________________________________');
+        console.log('_____________________________________');
+        console.log('_____________________________________');
+      } else if
+      (this.isEnemyNear()) {
+        this.battle();
+      } else {
+        this.moveForward();
+      }
+    }, 1500);
+  }
+
+  isEnemyNear() {
+    const random = Math.floor(Math.random() * 10);
+    if (random >= 4) return true;
+  }
+
+  battle() {
+    setTimeout(() => {
+      hero.checkAlive();
+      if (hero.healthPoints <= 10 && !hero.isAlive) {
+        const isWannaDrink = confirm('Looks like you have no HP\nWould you like to drink healing potion?');
+        if (isWannaDrink) hero.drinkHealingPot();
+        if (hero.isAlive) hero.healthPoints -= enemy.attack();
+        console.log('//////');
+        console.log(`${enemy.information.name} attacked you in 5 health points`);
+      } else if (!hero.isAlive) {
+        console.log('//////////////////');
+        console.log('You are dead. Game is over for you !');
+        console.log('//////////////////');
+      } else {
+        if (hero.isAlive) hero.healthPoints -= enemy.attack();
+        console.log('//////');
+        console.log(`${enemy.information.name} attacked you in 5 health points`);
+      }
+
+      if (enemy.isAlive && hero.isAlive) enemy.healthPoints -= hero.attack();
+      console.log('//////');
+      console.log(`You attacked ${enemy.information.name} with your ${hero.ammunition.firstWeapon}`);
+      console.log(`His HP - ${enemy.healthPoints} !`);
+
+      enemy.checkAlive();
+      if (enemy.isAlive === true && hero.isAlive) {
+        this.battle();
+      } else if (hero.isAlive) {
+        console.log(`${enemy.information.name} is dead!`);
+        this.state.killCount += 1;
+        enemy.healthPoints = 100;
+        enemy.isAlive = true;
+        console.log(`LvlUP ! Your level is ${hero.level}`);
+        this.statMessage();
+        this.moveForward();
+      }
+    }, 1500);
   }
 
   start() {
-    this.getName();
-    console.log(`Добро пожаловать в подземелье, ${hero.state.name}`);
-    console.log(`Твой текущий уровень равен ${hero.state.level}`);
-    this.gameOn();
-  }
-
-  gameOn() {
-    this.timerId = setInterval(() => {
-      console.log('Ты двигаешься по подземелью');
-      const tempRandom = Math.floor(Math.random() * 10);
-      if (tempRandom <= 5) {
-        this.state.isInBattle = true;
-      }
-      if (this.state.isInBattle) this.battleMode();
-    }, 3000);
-    console.log('Твой герой погиб, игра закончена !');
-  }
-
-  battleMode() {
-    clearInterval(this.timerId);
-    console.log(`Ты встретил противника ${enemy.state.name = 'Демонический прислужник'}`);
+    this.state.currentStep += 1;
+    this.welcomeMessage();
   }
 }
 
-const hero = new Hero();
-const enemy = new Enemy();
 const game = new Game();
 
-game.start();
+game.welcomeMessage();
