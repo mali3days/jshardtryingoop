000eb9f3c63ffa8444b16c1644f0df26f51cf190
Sergey Mogilevskiy <s.k.mogilevskiy@gmail.com>
2018-12-02 16:02:43 +0200

    Fix rewiew comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let register = {};
+const register = {};
 let id = 0;
 
 class Component {
@@ -6,50 +6,48 @@ class Component {
     this.id = id++;
     register[this.id] = this;
   }
-
 }
 
-let postDate = {
-  autor: "SomeAutor",
-  title: "title",
-  body: "Lorem ..."
+const postDate = {
+  autor: 'SomeAutor',
+  title: 'title',
+  body: 'Lorem ...',
 };
 
-let postDateWitcher = {
-  autor: "Andrzej Sapkowski",
-  title: "The Witcher",
-  body: "he Witcher, by Polish writer Andrzej Sapkowski, is a fantasy series of..."
-}; 
-
-
+const postDateWitcher = {
+  autor: 'Andrzej Sapkowski',
+  title: 'The Witcher',
+  body: 'he Witcher, by Polish writer Andrzej Sapkowski, is a fantasy series of...',
+};
 
 class Post extends Component {
   constructor({ autor, title, body }) {
     super();
-    this.state = { 
+    this.state = {
       autor,
       title,
-      body
-    }
+      body,
+    };
   }
 
   render() {
     return `<div data-id="${this.id}">
               <h2>${this.state.autor}</h2>
               <h3>${this.state.title}</h3>
-              <textarea onchange="register[${this.id}].update(this.value)">${this.state.body}</textarea>
+              <textarea onchange="register[${this.id}].update(this.value)">${
+  this.state.body
+}</textarea>
               <span>id-${this.id}</span>
             </div>`;
   }
-  
+
   update(value) {
     this.state.body = value;
-    document.querySelector("body").innerHTML = blogPost.render() + blogPostWitcher.render();
+    document.querySelector('body').innerHTML = blogPost.render() + blogPostWitcher.render();
   }
-
 }
 
 let blogPost = new Post(postDate);
 let blogPostWitcher = new Post(postDateWitcher);
 
-document.querySelector("body").innerHTML = blogPost.render() + blogPostWitcher.render();
+document.querySelector('body').innerHTML = blogPost.render() + blogPostWitcher.render();
--- src/game.js
+++ src/game.js
@@ -1,86 +1,86 @@
-let register = {};
+const register = {};
 let id = 0;
 
-let playerOne = {
-  name: "Player 1",
-  rase: "Human"
+const playerOne = {
+  name: 'Player 1',
+  rase: 'Human',
 };
-let playerTwo = {
-  name: "Player 2",
-  rase: "Gnome"
+const playerTwo = {
+  name: 'Player 2',
+  rase: 'Gnome',
 };
 
-// класс выполнения всех дейтсвий
 class Game {
   constructor() {
     this.score = { playerOne: 0, playerTwo: 0 };
   }
 
   createCharacter({ name, rase }) {
-    if (rase === "Gnome") {
+    if (rase === 'Gnome') {
       return new Gnome(name);
-    } else if (rase === "Human") {
+    }
+    if (rase === 'Human') {
       return new Human(name);
     }
   }
 
   initGame(player1, player2) {
-    // Создание 2 играков
     this.playerOne = this.createCharacter(player1);
     this.playerTwo = this.createCharacter(player2);
-    // Создания счета
-    document.body.innerHTML = "";
+    document.body.innerHTML = '';
     document.body.appendChild(this.render());
   }
 
   startFight() {
     const { playerTwo, playerOne } = this;
     let round = 1;
-    let list = document.createElement("ul");
+    const list = document.createElement('ul');
+    playerOne.health = 100;
+    playerTwo.health = 100;
 
     do {
+      const item = document.createElement('li');
+      item.innerHTML = `<h2>Round ${round}</h2>`;
 
-      let item = document.createElement("li");
-      item.innerHTML = `<h2>Round ${round}</h2>`
-
-      // Запись атак 1го игрока
       item.innerHTML += playerOne.attack(playerOne.damage, playerTwo);
       if (playerTwo.health <= 0) {
         item.innerHTML += `<h2>${playerOne.name} Win</h2>`;
-        list.appendChild(item)
+        list.appendChild(item);
         break;
       }
 
-      // Запись атак 2го игрока
       item.innerHTML += playerTwo.attack(playerTwo.damage, playerOne);
       if (playerOne.health <= 0) {
         item.innerHTML += `<h2>${playerTwo.name} win</h2>`;
-        list.appendChild(item)
+        list.appendChild(item);
         break;
       }
-      //возвращаем итог раунда
-      list.appendChild(item)
-      round++
+      item.innerHTML += `${playerOne.name} ${playerOne.health}/100<br>
+                        ${playerTwo.name} ${playerTwo.health}/100<br>`;
+      list.appendChild(item);
+      round++;
     } while (playerOne.health >= 0 || playerTwo.health >= 0);
 
-    document.getElementById("log").appendChild(list)
-    return 
+    document.getElementById('log').innerHTML = "";
+    document.getElementById('log').appendChild(list);
   }
 
   render() {
-    let div = document.createElement("div");
-    div.innerHTML = `
-      <h1>win:${this.score.playerOne} ${this.playerOne.name} VS  ${
-      this.playerTwo.name
-    } ${this.score.playerTwo}:win</h1>
-      <div id="log">
-        <button onclick=game.startFight()>Start Fight</button>
+    const div = document.createElement('div');
+
+    div.innerHTML = `<div>
+        <h1>win:${this.score.playerOne} ${this.playerOne.name} VS  ${this.playerTwo.name} ${
+  this.score.playerTwo
+}:win</h1>
+      <button onclick=game.startFight()>Start Fight</button>
+        <div id="log">
+        </div>
       </div>`;
+
     return div;
   }
 }
 
-// Классы персонажей
 class Character {
   constructor(name) {
     this.id = id++;
@@ -91,45 +91,42 @@ class Character {
       bow: {
         min: 1,
         max: 4,
-        attacks: 3
+        attacks: 3,
       },
       sword: {
-        min: 3,
-        max: 10,
-        attacks: 1
-      }
+        min: 4,
+        max: 12,
+        attacks: 1,
+      },
     };
   }
 
   attack({ min, max, attacks }, target) {
-    let resultAttack = ""
+    let resultAttack = '';
     let damage = 0;
 
     for (let i = 0; i < attacks; i++) {
-      let hit = Math.floor(Math.random() * (max + 1 - min)) + min;
+      const hit = Math.floor(Math.random() * (max + 1 - min)) + min;
       damage += hit;
-      resultAttack +=`${this.name} нанес ${hit} урона играку ${target.name}<br>` ;
+      resultAttack += `${this.name} ataks ${target.name}. ${target.name} takes ${hit} damage <br>`;
     }
     target.health -= damage;
-    resultAttack += `здоровья ${target.name} = ${target.health}<br>`
-    return resultAttack
+    return resultAttack;
   }
 }
 
 class Gnome extends Character {
   constructor(name) {
     super(name);
-    this.damage = this.weapons["bow"];
+    this.damage = this.weapons.bow;
   }
 }
 
 class Human extends Character {
   constructor(name) {
     super(name);
-    this.damage = this.weapons["sword"];
+    this.damage = this.weapons.sword;
   }
 }
 
 const game = new Game();
-
-
